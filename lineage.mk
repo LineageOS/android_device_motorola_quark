f06633763971a545f83c4e6dbf9767877a9efb1d
Felipe Leon <fglfgl27@gmail.com>
2017-04-30 18:12:54 -0300

    Quark: cm to lineage file/product name change + update proprietary-files and clean up
    Change-Id: I895f610c1309d99b4602a1ed80f309d606d27a2e
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -7,6 +7,6 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_RELEASE_NAME := Moto MAXX
-PRODUCT_NAME := cm_quark
+PRODUCT_NAME := lineage_quark
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
