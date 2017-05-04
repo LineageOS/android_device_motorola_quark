20d810e98a4c712b6d44f0a0e22e99e25e1678ae
Felipe Leon <fglfgl27@gmail.com>
2017-05-04 09:43:43 -0300

    Quark: device.mk add webview permission
    Change-Id: Ibda08e6734d16612e6d71a08c29b35b29b021b88
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- device.mk
+++ device.mk
@@ -62,6 +62,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml \
+    frameworks/native/data/etc/android.software.webview.xml:system/etc/permissions/android.software.webview.xml
 
 $(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-dalvik-heap.mk)
 
