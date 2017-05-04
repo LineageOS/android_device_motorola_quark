b8aa75b762c70dc1923d800758525d0e6a4c2c75
Felipe Leon <fglfgl27@gmail.com>
2017-05-04 09:31:45 -0300

    Quark: add gms clientid base
    * needed for correct configuration for play
    
    Change-Id: I5a06cbabf12c0b37f1f23cdfd9bc801d93995ecb
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -11,3 +11,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_RELEASE_NAME := Moto MAXX
 PRODUCT_NAME := lineage_quark
+
+#needed for play
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
