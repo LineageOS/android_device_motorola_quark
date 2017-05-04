48eb953819479f9fd4958ba921766d29a1bfc426
Felipe Leon <fglfgl27@gmail.com>
2017-05-04 09:45:37 -0300

    Quark: device.mk update dalvik-heap and hwui-memory
    This is the best value for quark on N
    override the values for dalvik here because minor change on the common file can cause bootloops
    
    Change-Id: I2ef5267823912fa08d9cd1237c025ab62236bb29
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- device.mk
+++ device.mk
@@ -65,9 +65,16 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.webview.xml:system/etc/permissions/android.software.webview.xml \
     frameworks/native/data/etc/android.software.voice_recognizers.xml:system/etc/permissions/android.software.voice_recognizers.xml
 
-$(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-dalvik-heap.mk)
+# Dalvik.vm size adapted for quark on N
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
 
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxxhdpi-3072-hwui-memory.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
 # Audio
 PRODUCT_PACKAGES += \
